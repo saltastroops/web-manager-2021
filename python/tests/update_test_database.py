e201d85af4d9a826641718544ee3f3e500f0c3cc
Christian Hettlage <hettlage@saao.ac.za>
2021-02-24 10:28:02 +0200

    Refactored code (in progress)

--- python/tests/update_test_database.py
+++ python/tests/update_test_database.py
@@ -1,29 +1,56 @@
+import asyncio
 import os
+import pathlib
+import subprocess
+import tempfile
 from random import randint
 from typing import List
 
 import click
+import pydantic
 from aiomysql import connect
 from faker import Faker
-
 
 fake = Faker()
 Faker.seed(4321)
 
 
+class Database(pydantic.BaseModel):
+    host: str
+    database: str
+    username: str
+    password: str
+
+
+class TargetCoordinates(pydantic.BaseModel):
+    """Right ascension and declination."""
+
+    id: int
+    ra_h: int
+    ra_m: int
+    ra_s: float
+    dec_sign: str
+    dec_deg: int
+    dec_m: int
+    dec_s: float
+
+
 async def get_all_investigator_ids(test_db_connection: connect) -> List[int]:
     """
     Get all the investigators ids.
+
     Parameters
     ----------
     test_db_connection:
-        The database connection that have table Investigator as the table.
+        The test database connection.
 
     Returns
     -------
-        A List of investigators ids.
+    list
+        The List of investigators ids.
 
     """
+
     with test_db_connection.cursor() as cur:
         cur.execute("SELECT Investigator_Id FROM Investigator")
         results = await cur.fetchall()
@@ -32,26 +59,30 @@ async def get_all_investigator_ids(test_db_connection: connect) -> List[int]:
 
 async def get_all_pipt_user_ids(test_db_connection: connect) -> List[int]:
     """
-    Get all the pipt users ids.
+    Get all the PIPT user ids.
+
     Parameters
     ----------
     test_db_connection:
-        The database connection that have table PiptUser as the table.
+        The test database connection.
 
     Returns
     -------
-        A List of PIPT user ids.
+    list
+        The List of PIPT user ids.
 
     """
+
     with test_db_connection.cursor() as cur:
         cur.execute("SELECT PiptUser_Id FROM PiptUser")
         results = await cur.fetchall()
         return [pipt_user[0] for pipt_user in results]
 
 
-async def get_all_target_coordinates_id(test_db_connection: connect) -> List[int]:
+async def get_all_target_coordinate_ids(test_db_connection: connect) -> List[int]:
     """
-    Get all the target coordinates ids.
+    Get all the target coordinate ids.
+
     Parameters
     ----------
     test_db_connection:
@@ -59,94 +90,122 @@ async def get_all_target_coordinates_id(test_db_connection: connect) -> List[int
 
     Returns
     -------
-        A List of target coordinates ids.
+    list
+        The list of target coordinates ids.
 
     """
-    target_coordinates_ids = []
+
     with test_db_connection.cursor() as cur:
         cur.execute("SELECT TargetCoordinates_Id FROM TargetCoordinates")
         results = await cur.fetchall()
-        return [target_coordinate[0] for target_coordinate in results]
+        return [target_coordinates[0] for target_coordinates in results]
 
 
-async def update_investigators(test_db_connection: connect, db_cursor: connect().cursor) -> None:
+async def update_investigators(test_db_connection: connect) -> None:
     """
-    Generate a query that will update all the sensitive information from the Investigator table.
+    Replace the sensitive data in the Investigator table with fake data.
 
     Parameters
     ----------
     test_db_connection
-        The test database connection
-    db_cursor
-        The Test database cursor
+        The test database connection.
 
-    Returns
-    -------
-        None
     """
+
     investigator_ids = await get_all_investigator_ids(test_db_connection)
-    for investigator_id in investigator_ids:
-        sql = f"""Update Investigator
-SET  FirstName = {fake.first_name()}, Surname = {fake.last_name()}, Email = {fake.email()}, Phone = {fake.phone_number()}
-WHERE Investigator_Id = {investigator_id};
+    data = [
+        (
+            fake.first_name(),
+            fake.last_name(),
+            fake.email(),
+            fake.phone_number(),
+            investigator_id,
+        )
+        for investigator_id in investigator_ids
+    ]
+    sql = f"""
+UPDATE Investigator
+SET  FirstName = %s, Surname = %s, Email = %s, Phone = %s
+WHERE Investigator_Id = %s;
     """
-        db_cursor.execute(sql)
-        test_db_connection.commit()
+    cursor = await test_db_connection.cursor()
+    await cursor.executemany(sql, data)
 
 
-async def update_pipt_users_query(test_db_connection: connect, db_cursor: connect().cursor) -> None:
+async def update_pipt_users(test_db_connection: connect) -> None:
     """
-    Generate a query that will update all the sensitive information from the PIPT user table.
+    Replace the sensitive data in the PiptUser table with fake data.
 
     Parameters
     ----------
     test_db_connection
-        The test database connection
-    db_cursor
-        The Test database cursor
+        The test database connection.
 
-    Returns
-    -------
-        None
     """
+
     pipt_user_ids = await get_all_pipt_user_ids(test_db_connection)
-    for pipt_user_id in pipt_user_ids:
-        sql = f"""UPDATE PiptUser
-SET Username = user-{fake.user_name()}, Password = MD5(user-{pipt_user_id}-{os.getenv("TEST_DB_USER_PASSPHRASE")})
-WHERE PiptUser_Id = {pipt_user_id};
+    passphrase = os.getenv("TEST_DB_USER_PASSPHRASE")
+    data = [
+        (fake.user_name(), f"user-{pipt_user_id}-{passphrase}", pipt_user_id)
+        for pipt_user_id in pipt_user_ids
+    ]
+    sql = f"""
+UPDATE PiptUser
+SET Username = %s, Password = MD5(%s)
+WHERE PiptUser_Id = %s;
     """
-        db_cursor.execute(sql)
-        test_db_connection.commit()
+    cursor = await test_db_connection.cursor()
+    await cursor.executemany(sql, data)
 
 
-async def update_target_coordinates_query(test_db_connection: connect, db_cursor: connect().cursor) -> None:
+def fake_target_coordinates(target_coordinates_id: int) -> TargetCoordinates:
+    """Generate fake target coordinates."""
+    dec_deg = randint(-75, 10)
+    sign = "-" if dec_deg < 0 else "+"
+    return TargetCoordinates(
+        id=target_coordinates_id,
+        ra_h=randint(0, 23),
+        ra_m=randint(0, 59),
+        ra_s=randint(0, 59),
+        dec_sign=sign,
+        dec_deg=abs(dec_deg),
+        dec_m=randint(0, 59),
+        dec_s=randint(0, 59),
+    )
+
+
+async def update_target_coordinates(test_db_connection: connect) -> None:
     """
-    Generate a query that will update all the sensitive information from the target coordinates table.
+    Replace the sensitive data in the TargetCoordinates table with fake data.
 
     Parameters
     ----------
     test_db_connection
-        The test database connection
-    db_cursor
-        The Test database cursor
+        A test database connection.
 
-    Returns
-    -------
-        None
     """
-    target_coordinates_ids = await get_all_target_coordinates_id(test_db_connection)
-    for target_coordinates_id in target_coordinates_ids:
-        dec_h = randint(-75, 10)
-        sign = "-" if dec_h < 0 else "+"
-        sql = f"""UPDATE TargetCoordinates
-SET RaH={randint(0, 23)},RaM={randint(0, 59)},RaS={randint(0, 59)},DecSign={sign},DecD={abs(dec_h)},
-DecM={randint(0, 59)},DecS={0, 59} WHERE TargetCoordinates_Id = {target_coordinates_id};      
+
+    target_coordinate_ids = await get_all_target_coordinate_ids(test_db_connection)
+    target_coordinate_list = [
+        fake_target_coordinates(target_coordinate_id)
+        for target_coordinate_id in target_coordinate_ids
+    ]
+    data = [
+        (tc.ra_h, tc.ra_m, tc.ra_s, tc.dec_sign, tc.dec_deg, tc.dec_m, tc.dec_s, tc.id)
+        for tc in target_coordinate_list
+    ]
+    sql = f"""
+UPDATE TargetCoordinates
+SET RaH=%s, RaM=%s, RaS=%s, DecSign=%s, DecD=%s, DecM=%s, DecS=%s
+WHERE TargetCoordinates_Id=%s;      
     """
-        db_cursor.execute(sql)
-        test_db_connection.commit()
+    cursor = await test_db_connection.cursor()
+    await cursor.executemany(sql, data)
 
 
-def create_empty_test_database(test_db_connection: connect, test_db_name: str) -> None:
+async def create_empty_test_database(
+    test_db_connection: connect, test_db_name: str
+) -> None:
     """
     Create an empty test database.
 
@@ -156,109 +215,156 @@ def create_empty_test_database(test_db_connection: connect, test_db_name: str) -
     Parameters
     ----------
     test_db_connection
-        The test database connection.
+        A test database connection.
     test_db_name
         The test database name, which must start with "test".
 
-    Returns
-    -------
-        None
     """
 
     if not test_db_name.startswith("test"):
-        raise ValueError("The test database name must start with \"test\".")
+        raise ValueError('The test database name must start with "test".')
 
-    try:
-        with test_db_connection.cursor() as cur:
-            # Delete an existing database
-            drop_query = f"DROP DATABASE IF EXISTS %(db_name)s"
-            cur.execute(drop_query, {"db_name": test_db_name})
+    # Delete an existing database
+    async with test_db_connection.cursor() as cur:
+        drop_query = f"DROP DATABASE IF EXISTS `{test_db_name}`"
+        await cur.execute(drop_query)
 
-            # Create the database
-            create_query = f"CREATE DATABASE %(db_name)s"
-            cur.execute(create_query, {"db_name": test_db_name})
-    finally:
-        test_db_connection.commit()
+        # Create the database
+        create_query = f"CREATE DATABASE `{test_db_name}`"
+        await cur.execute(create_query)
 
 
-def update_sensitive_info(test_db_connection: connect) -> None:
-    # !!! INITIATE A TRANSACTION !!!
-    try:
-        test_db_connection.begin()
-        with test_db_connection.cursor() as cur:
-            cur.execute(update_investigators(test_db_connection, cur))
-            cur.execute(update_pipt_users_query(test_db_connection, cur))
-            cur.execute(update_target_coordinates_query(test_db_connection, cur))
-
-    except Exception as e:
-        print("Exception occurred:{}".format(e))
-    finally:
-        test_db_connection.commit()
-        test_db_connection.close()
+async def replace_sensitive_info(test_db_connection: connect) -> None:
+    """Replace sensitive information in the test database with fake data."""
+    await update_investigators(test_db_connection)
+    await update_pipt_users(test_db_connection)
+    await update_target_coordinates(test_db_connection)
 
 
-def dump_database(db_user, db_password, db_host, db_name, result_file):
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    dump_file = script_dir + "/sdb_copy.sql"
-
-    # Dumping the main database
-    os.system(
-        f"mysqldump -u {db_user} --password {db_password} -h {db_host} "
-        f"--default-character-set=utf8 --routines --ignore-table=bumping_sdb.log {db_name} "
-        f"--result-file {result_file}"
+def dump_database(db_user, db_password, db_host, db_name, dump_file):
+    subprocess.run(
+        [
+            "mysqldump",
+            "-u",
+            db_user,
+            f"-p{db_password}",
+            "-h",
+            db_host,
+            "--default-character-set=utf8",
+            "--routines",
+            "--single-transaction",
+            "-r",
+            dump_file,
+            db_name,
+        ]
     )
+
+
+async def create_test_database(main_db: Database, test_db: Database) -> None:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        dump_file = pathlib.Path(tmp_dir) / "dump.sql"
+
+        # # dump the main database
+        # dump_database(
+        #     db_user=main_db.username,
+        #     db_password=main_db.password,
+        #     db_host=main_db.host,
+        #     db_name=main_db.database,
+        #     dump_file=dump_file
+        # )
+
+        dump_file = "/Users/christian/Desktop/sdb.sql"
+
+        # create the test database
+        # Note: The name of the test database name must not be passed, as the database
+        #       might not exist yet.
+        test_db_server_connection = await connect(
+            host=test_db.host, user=test_db.username, password=test_db.password
+        )
+        await create_empty_test_database(test_db_server_connection, test_db.database)
+        test_db_server_connection.close()
+
+        # importing mysql dump file
+        os.system(
+            [
+                f"mysql -u {test_db.username} -p{test_db.password} -h {test_db.host} "
+                f"--default-character-set=utf8 {test_db.database} < {dump_file}"
+            ]
+        )
+        #
+        # # Connect to the newly created test database
+        #
+        # script_dir = os.path.dirname(os.path.realpath(__file__))
+        #
+        # dump_file = script_dir + "/sdb_copy.sql"
+        #
+        # # Remove the test database
+        # os.system(f"rm {dump_file}")
+        #
+        # # Update the test database
+
+        # connect to the test database (now we have to pass the name)
+        test_db_connection = await connect(
+            host=test_db.host, user=test_db.username, password=test_db.password
+        )
+
+        # replace_sensitive_info(test_db_connection)
 
 
 @click.command()
-@click.option('--main-db-host', type=str, help="The database to be dumped for this script it is the SDB.")
-@click.option('--main-db-user', type=str, help="The main database user.")
-@click.option('--main-db-name', type=str, help="The main database name.")
-@click.option('--test-db-host', type=str, help="The test database host.")
-@click.option('--test-db-user', type=str, help="The test database host.")
-@click.option('--test-db-name', type=str, help="This is a new database for testing the name should start with test."
-                                               "")
-@click.option('--main-db-password', prompt="Enter main database password:", hide_input=True, confirmation_prompt=False,
-                type=str)
-@click.option('--test-db-password', prompt="Enter test database password:", hide_input=True, confirmation_prompt=False,
-              type=str)
+@click.option(
+    "--main-db-host",
+    type=str,
+    help="The database to be dumped for this script it is the SDB.",
+)
+@click.option("--main-db-user", type=str, help="The main database user.")
+@click.option("--main-db-name", type=str, help="The main database name.")
+@click.option("--test-db-host", type=str, help="The test database host.")
+@click.option("--test-db-user", type=str, help="The test database host.")
+@click.option(
+    "--test-db-name",
+    type=str,
+    help="This is a new database for testing the name should start with test." "",
+)
+@click.option(
+    "--main-db-password",
+    prompt="Enter main database password:",
+    hide_input=True,
+    confirmation_prompt=False,
+    type=str,
+)
+@click.option(
+    "--test-db-password",
+    prompt="Enter test database password:",
+    hide_input=True,
+    confirmation_prompt=False,
+    type=str,
+)
 def cli(
-        main_db_host,
-        main_db_user,
-        main_db_name,
-        test_db_host,
-        test_db_user,
-        test_db_name,
-        main_db_password,
-        test_db_password,
+    main_db_host,
+    main_db_user,
+    main_db_name,
+    test_db_host,
+    test_db_user,
+    test_db_name,
+    main_db_password,
+    test_db_password,
 ):
-    # Connect to the newly created test database
-    test_db_connection = connect(host=test_db_host, user=test_db_user, password=test_db_password, db=test_db_name,
-                                 port=3306)
-
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    dump_file = script_dir + "/sdb_copy.sql"
-
-    # Dumping the main database
-    dump_database(
-        db_user=main_db_user,
-        db_password=main_db_password,
-        db_host=main_db_host,
-        db_name=main_db_name,
-        result_file=dump_file
+    main_db = Database(
+        host=main_db_host,
+        database=main_db_name,
+        username=main_db_user,
+        password=main_db_password,
+    )
+    test_db = Database(
+        host=test_db_host,
+        database=test_db_name,
+        username=test_db_user,
+        password=test_db_password,
     )
 
-    # create the test database
-    create_empty_test_database(test_db_connection, test_db_name)
+    asyncio.run(create_test_database(main_db, test_db))
 
-    # importing mysql dump file
-    os.system(
-        f"mysql -u {test_db_user} --password='{test_db_password}' -h {test_db_host} "
-        f"--default-character-set=utf8 {test_db_name} < {dump_file}"
-    )
-    # Remove the test database
-    os.system(f"rm {dump_file}")
 
-    # Update the test database
-    update_sensitive_info(test_db_connection)
+if __name__ == "__main__":
+    cli()
