1c9d76e551fa6ea2fd5310f103fc09a594bbe21f
Eb-Zeero <macebelene@gmail.com>
2021-03-10 15:17:26 +0200

    reading a xml file and returning a xml in a html tag.

--- python/app/service/block.py
+++ python/app/service/block.py
@@ -1,2 +1,24 @@
+import os
+import zipfile
+from typing import List
+
+
+def get_last_submission(proposal_code: str) -> List[str]:
+    return sorted(
+        [f.path for f in os.scandir(f"""{os.getenv("BASE_DIR")}/{proposal_code}""") if f.is_dir()]
+    )[-2]
+
+
 def get_block_html(proposal_code: str, block_code: str) -> str:
-    return f"<div>Block {block_code} (in proposal {proposal_code})</div>"
+    last_submission = get_last_submission(proposal_code)
+    archive = zipfile.ZipFile(f"{last_submission}/{proposal_code}.zip", 'r')
+    proposal_xml = archive.open('Proposal.xml')
+    return f"""
+<pre>
+    ProposalCode: {proposal_code}    Block: {block_code}
+    ____________________________________________________________
+    
+    {proposal_xml.read()}
+    
+</pre>
+"""
