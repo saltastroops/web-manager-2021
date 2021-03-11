7ba1b72cbc35f8060dd7a97e05fb26a2557b0ec6
Eb-Zeero <macebelene@gmail.com>
2021-03-11 11:12:46 +0200

    remove a static block file

--- python/app/service/block.py
+++ python/app/service/block.py
@@ -1,17 +1,18 @@
 import aiofiles
 import os
-from typing import List
+from glob import glob
 
 
-def get_last_submission(proposal_code: str) -> List[str]:
-    return sorted(
-        [f.path for f in os.scandir(f"""{os.getenv("BASE_DIR")}/{proposal_code}""") if f.is_dir()]
-    )[-2]
+def get_block_file(proposal_code: str, block_code:str) -> str:
+    block = glob(f"""{os.getenv("PROPOSALS_BASE_DIR")}/{proposal_code}/Included/Block-{block_code}-*""")
+    if len(block) < 1:
+        raise ValueError(f"Block: {block_code } not found")
+    return block[0]
 
 
 async def get_block_html(proposal_code: str, block_code: str) -> str:
-    last_submission = f"""{os.getenv("BASE_DIR")}/{proposal_code}/Included/Block-{block_code}-2018-1.xml"""
-    with aiofiles.open(last_submission, mode='r') as f:
+    block_xml = get_block_file(proposal_code, block_code)
+    async with aiofiles.open(block_xml, mode='r') as f:
         contents = await f.read()
         return f"""
 <pre>
